c838b6b25dd2053a063330cb263d686a1c19f9ef
masaru <mseik83@hotmail.co.jp>
2018-06-10 22:36:12 +0900

    デフォルトコンストラクタ、コンストラクタ、コピーコンストラクタの実装。 等価演算子の実装。

--- Complex.cpp
+++ Complex.cpp
@@ -8,9 +8,26 @@ using C = math::complex::Complex<T>;
 template <typename T>
 using R = math::rational::Rational<T>;
 
+class Test {
+public:
+  //constexpr Test(int x) { throw 0; }
+  constexpr Test(int x) {}
+  constexpr Test(const Test& other) { throw 0; };
+};
+
 
 int main()
 {
-  std::cout << C<R<int>>() << std::endl;
+  std::cout << C<int>() << std::endl;
+  std::cout << C<R<int>>(R<int>(1, 2)) << std::endl;
+  std::cout << C<int>(1, 2) << std::endl;
+  static_assert(C<R<int>>() == C<R<int>>(0), "");
+  static_assert(C<int>(2) == C<int>(2, 0), "");
+  //static_assert(!noexcept(C<Test>()), "");
+  //static_assert(noexcept(C<R<int>>()), "");
+  static_assert(!noexcept(C<Test>(3)), "");
+  static_assert(noexcept(C<R<int>>(3)), "");
+  static_assert(!noexcept(C<Test>(3, 5)), "");
+  static_assert(noexcept(C<R<int>>(R<int>(3), R<int>(5))), "");
   return 0;
 }
--- Complex.hpp
+++ Complex.hpp
@@ -5,12 +5,44 @@
 
 namespace math {
 namespace complex {
+
 template <typename T>
 class Complex {
 public:
   friend std::ostream& operator <<(std::ostream& os, const Complex& x) {
     return os << x.real << "+" << x.imag << "i";
   }
+
+
+public:
+  //------------------------------
+  //
+  //  コンストラクタ
+  //
+  //------------------------------
+  //デフォルトコンストラクタ。要素型の零を引数にとるコンストラクタがnoexceptなら、noexcept。
+  constexpr Complex() noexcept(noexcept(T(0))) {}
+  //コンストラクタ。要素型のコピーコンストラクタがnoexceptならnoexcept。
+  constexpr Complex(T r) noexcept(noexcept(T(T(0)))) : real(r) {}
+  constexpr Complex(T r, T i) noexcept(noexcept(T(T(0)))) : real(r), imag(i) {}
+  //コピーコンストラクタ。
+  constexpr Complex(const Complex& other) noexcept = default;
+
+
+public:
+  //------------------------------
+  //
+  //  論理演算子
+  //
+  //------------------------------
+  constexpr bool operator ==(const Complex& rhs) const {
+    return real == rhs.real && imag == rhs.imag;
+  }
+
+  constexpr bool operator !=(const Complex& rhs) const {
+    return !(*this == rhs);
+  }
+
 
 protected:
   T real = static_cast<T>(0);
